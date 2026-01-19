02c83e5aaf9c532a48ec2ec3a972c5fbc390e1c0
Shivani Lowanshi <lowanshishivani45@gmail.com>
2026-01-19 18:32:23 +0000

    added conversionOperation

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,47 @@
+let score = 33
+
+//  console.log(typeof score);       // number
+// console.log(typeof(score));       // number
+
+let age = "22"
+// console.log(typeof age);    // string
+//  console.log(age)    // 22
+
+let valueInNumber = Number(age)      
+// console.log(typeof valueInNumber);    //  number
+// console.log(valueInNumber);         //  22
+
+let val = "33abc"
+let valInNumber = Number(val)
+//  console.log(typeof valInNumber);     // number
+//  console.log(valInNumber);            // NaN
+
+
+let A = null   
+// console.log(typeof(A));            // object    
+let B = Number(A)
+// console.log(typeof(B));           // number
+// console.log(B);                  // 0
+
+
+let value_1 = undefined
+console.log(typeof(value_1));        // undefined
+
+let value_2 = Number(value_1);      
+console.log(typeof(value_2));        // number
+console.log(value_2);                // NaN
+
+
+let value_3 = true
+console.log(typeof(value_3));     // boolean
+let value_4 = Number(value_3);
+console.log(typeof(value_4))      // number
+console.log(value_4)             //  1
+
+
+let str = "Shivani"
+console.log(typeof(str));     // string
+let str2 = Number(str);
+console.log(typeof(str2));    // number
+console.log(str2);            // NaN
+
